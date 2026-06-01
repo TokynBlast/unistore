ab816107fcf555763938a67a1e2f6ed5cabf22a0
RuriMei <tokynblast@gmail.com>
2026-06-01 14:37:58 -0700

    Remove all but unistore and README

--- build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-  let devkitpro = std::env::var("DEVKITPRO").unwrap_or_else(|_| "/opt/devkitpro".to_string());
-
-  // Pass the search path for libctru binary to cargo
-  println!("cargo:rustc-link-search=native={}/libctru/lib", devkitpro);
-  println!("cargo:rustc-link-lib=static=ctru");
-}
